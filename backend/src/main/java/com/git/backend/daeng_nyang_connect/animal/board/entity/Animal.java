7bb12408acd02d5efb133c338f50120beb83ad06
이민수 <cha1717200@gmail.com>
2023-12-01 14:33:54 +0900

    Feat : 파양동물 api 기본 구상

--- backend/src/main/java/com/git/backend/daeng_nyang_connect/animal/board/entity/Animal.java
+++ backend/src/main/java/com/git/backend/daeng_nyang_connect/animal/board/entity/Animal.java
@@ -1,5 +1,6 @@
-package com.git.backend.daeng_nyang_connect.animal.board.entity;
+package com.git.backend.daeng_nyang_connect.animal.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.git.backend.daeng_nyang_connect.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -19,7 +21,7 @@ import java.util.List;
 @Table(name = "animal")
 public class Animal {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "animal_idx")
+    @Column(name = "animal_board_idx")
     private Long animalId;
 
     @ManyToOne
@@ -32,10 +34,24 @@ public class Animal {
     private Integer age;
     private String gender;
     private String disease;
-    private String breed;
+    private String breed; // 종
+
+    @Column(name = "text_reason")
+    private String textReason; // 파양 이유
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "nurture_period")
+    private Date nurturePeriod; // 입양 시기
+
     private String training;
-    private Boolean neutering;
-    private String contents;
+    private Boolean neutering; // 중성화 여부
+
+    @Enumerated(EnumType.STRING)
+    private Kind kind; // 강아지 or 고양이 or 기타
+
+    @Column(name = "adoption_status")
+    @Enumerated(EnumType.STRING)
+    private AdoptionStatus adoptionStatus; // 입양 완료 여부
 
     @Column(name = "health_check")
     private String healthCheck;
@@ -43,9 +59,13 @@ public class Animal {
     @Column(name = "created_at")
     private Timestamp createdAt;
 
-    @Column(name = "nurture_period")
-    private String nurturePeriod;
+    private String city;
 
-    @OneToMany(mappedBy = "animal", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<AnimalImage> images;
+    @OneToMany(mappedBy = "animal", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<AnimalImage> images = new ArrayList<>();
+
+    public void updateAdoptionStatus(AdoptionStatus adoptionStatus){
+        this.adoptionStatus = adoptionStatus;
+    }
 }
---
+++ backend/src/main/java/com/git/backend/daeng_nyang_connect/animal/board/repository/AnimalImageRepository.java
@@ -0,0 +1,7 @@
+package com.git.backend.daeng_nyang_connect.animal.board.repository;
+
+import com.git.backend.daeng_nyang_connect.animal.board.entity.AnimalImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimalImageRepository extends JpaRepository<AnimalImage, Long> {
+}
