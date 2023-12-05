ad536df4a7933a7bde655141eafafc8f13e773cb
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-12-05 13:03:02 +0900

    user 서비스 개발 중

--- backend/src/main/java/com/git/backend/daeng_nyang_connect/jwt/TokenProvider.java
+++ backend/src/main/java/com/git/backend/daeng_nyang_connect/jwt/TokenProvider.java
@@ -1,6 +1,11 @@
 package com.git.backend.daeng_nyang_connect.jwt;
 
 import com.git.backend.daeng_nyang_connect.user.repository.CustomerUserDetails;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.openid.connect.sdk.claims.ClaimsSet;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -24,6 +30,20 @@ public class TokenProvider {
     @PostConstruct
     protected void init(){
         secretKey  = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    private String getString(String email, long accesstokenValidSecond){
+        Claims claims = Jwts.claims().setSubject(email);
+        Date now = new Date();
+        Date accessValidate = new Date(now.getTime() + accesstokenValidSecond);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setExpiration(accessValidate)
+                .setIssuedAt(now)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
     }
 
 
