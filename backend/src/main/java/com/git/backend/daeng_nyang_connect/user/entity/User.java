2889ab268789b0db247fbc8fd57cfd10826a1fd2
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-12-06 14:24:15 +0900

    로그인 수정중

--- backend/src/main/java/com/git/backend/daeng_nyang_connect/user/entity/User.java
+++ backend/src/main/java/com/git/backend/daeng_nyang_connect/user/entity/User.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
@@ -38,6 +39,7 @@ public class User implements UserDetails {
     private Boolean experience;
 
     @Enumerated(EnumType.ORDINAL)
+    @Column(name = "role")
     private Role role;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
@@ -46,6 +48,7 @@ public class User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(this.role.getName()));
         return authorities;
     }
 
