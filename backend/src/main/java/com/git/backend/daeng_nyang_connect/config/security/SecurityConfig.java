f76799b76c29d2c8eeaa17c695f516fd7a95dbc2
이민수 <cha1717200@gmail.com>
2024-01-05 10:14:30 +0900

    fix : chat

--- backend/src/main/java/com/git/backend/daeng_nyang_connect/config/security/SecurityConfig.java
+++ backend/src/main/java/com/git/backend/daeng_nyang_connect/config/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
